654856332ce742fb2d3b7637fe072de2535d43b2
ozlemcan5 <115418399+ozlemcan5@users.noreply.github.com>
2026-03-29 18:30:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    val_port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=val_port, debug=False)
